5886a260def119861361ee218f13da249c1d4c84
Kathirmani Sukumar <kathir.mani@gramener.com>
2016-01-23 15:57:09 +0530

    Sample jupyter notebook added

--- class_intro.py
+++ class_intro.py
@@ -1,15 +1,19 @@
-
 class Person:
   # Variables
   # Functions
 
   def __init__(self, name, initial_deposit):
+    # print 'initialized'
     self.name = name
     self.initial_deposit = initial_deposit
     self.balance = initial_deposit
+    self.__total_accounts = 10001
 
   def current_balance(self):
     return self.balance
+
+  def total_accounts(self):
+    print  self.__total_accounts
 
   def withdraw(self, amount):
     self.balance = self.balance - int(amount)
@@ -17,3 +21,8 @@ class Person:
 
   def deposit(self, amount):
     self.balance = self.balance + int(amount)
+
+  def __del__(self):
+    pass
+    # print 'This instance is deleted; will not exists anymore'
+
